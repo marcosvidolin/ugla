fe71dc69fc9e75cbd0d18b42080192b3d5eda9cc
Regivaldo Silva <regivaldorfs@gmail.com>
2020-02-04 10:38:49 -0300

    Clean tests

--- src/app/pages/dev-page/dev-page.component.ts
+++ src/app/pages/dev-page/dev-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Options } from 'projects/ugla/src';
 
 @Component({
   selector: 'app-dev-page',
@@ -7,40 +6,8 @@ import { Options } from 'projects/ugla/src';
   styleUrls: ['./dev-page.component.scss']
 })
 export class DevPageComponent implements OnInit {
-
-  list = [
-    new Options('Teste 1', '1'),
-    new Options('Teste 2', '2'),
-    new Options('Teste 3', '3'),
-    new Options('Teste 4', '4'),
-  ];
-
   constructor() { }
 
   ngOnInit() {
   }
-
-  onInitDatepicker() {
-    return {
-      startDate: new Date(),
-      position: 'br'
-    };
-  }
-
-  onChangeInitialDate(date: Date) {
-    console.log(date);
-  }
-
-  radioChecked(item: Options) {
-    console.log(item);
-    console.log(`O item ${item.description} foi selecionado e seu valor é ${item.value}.`);
-  }
-
-  cardClick($event: any) {}
-
-  lightboxAction(event: string) {
-    console.log('Teste', event);
-    return false;
-  }
-
 }
