1e27858cdfa93f5d63377a87765dad5bae050b38
Regivaldo Silva <regivaldorfs@gmail.com>
2020-02-04 09:48:50 -0300

    Fix sass lint

--- projects/ugla/src/lib/components/banner/banner.component.ts
+++ projects/ugla/src/lib/components/banner/banner.component.ts
@@ -17,73 +17,32 @@ import { Form } from '../../enum';
 })
 export class BannerComponent implements AfterViewInit {
 
-  /**
-   * @ignore
-   */
   constructor(private ugla: UglaService, private element: ElementRef) {
     this.color = !!this.color ? this.color : this.ugla.color;
   }
 
-  /**
-   * Receives the banner's title
-   */
   @Input() title: string;
-
-  /**
-   * Receives the banner's message
-   */
   @Input() message: string;
-
-  /**
-   * Receives the banner's color.
-   * Default is theme color
-   */
   @Input() color: string;
-
-  /**
-   * Receives the close button title.
-   * Default: Close
-   */
   @Input() closeButtonTitle = Form.CLOSE_BUTTON_LABEL;
-
-  /**
-   * Indicates if banner is open.
-   * Default: true
-   */
   @Input() open = true;
 
-  /**
-   * Emitter for close event
-   */
   @Output() close: EventEmitter<any> = new EventEmitter();
 
-  /**
-   * Executed after component is initialized. Focus on banner's title.
-   */
   ngAfterViewInit() {
-    (this.element.nativeElement as HTMLDivElement).querySelector<HTMLBodyElement>('#title').focus();
+    this.element.nativeElement.querySelector('h2').focus();
   }
 
-  /**
-   * Get banner classes
-   */
   getClasses() {
     return `${this.color} ${this.open ? '' : 'closed'}`;
   }
 
-  /**
-   * Opens banner programatically
-   */
   openBanner() {
     this.open = true;
   }
 
-  /**
-   * Close banner on close button click, and emits the close event
-   */
   closeBanner() {
     this.open = false;
     this.close.emit();
   }
-
 }
